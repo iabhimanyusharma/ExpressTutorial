6e5a68a01d74f8898e11f19fbe71a97734c90a88
iabhimanyusharma <81915715+iabhimanyusharma@users.noreply.github.com>
2022-04-22 16:36:52 +0530

    Update

--- final/04-express-app.js
+++ final/04-express-app.js
@@ -1,19 +1,23 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path'); // used to require the absolute path of the file in demand
 
-const app = express()
+const app = express(); // app here now is an object with various properties of express
 
-// setup static and middleware
 app.use(express.static('./public'))
+// app.use is the property used as an middleware
+//public folder consists of all the static pages like styles and javascript
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+app.get('/', (req, res)=>{
+  res.status(200).sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+}) // path.resolve tells the absolute path of the file, // we could aslo use path.join here
+
+app.all('*', (req, res)=>{
+  res.status(404).send('Resource Not Found')
 })
 
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
-})
+app.listen(5000, ()=>{
+console.log('Server is listening on port 5000')}); // app.listen is one of the properties of object of express
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
-})
+
+
+
